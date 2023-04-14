c171c139960cf1a7f61def264de7b831a83b543c
clauterra <38335060+clauterra@users.noreply.github.com>
2023-04-14 15:59:24 -0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,3 +1,3 @@
-var googleDocURL='https://docs.google.com/spreadsheets/d/1FRMs6esCZVDiFJgeMJWBv-Xlfnqh_JdFo_52dCMArH4/edit#gid=70869349'
+var googleDocURL='https://docs.google.com/spreadsheets/d/1_7p8ppgM4lrFchxrVtrzCaS2zYPK3W9rSEI2rRDP5W8/edit#gid=0'
 //google sheet API key from https://console.developers.google.com
 var googleApiKey='AIzaSyCfZ7Hx3UW7klKj6d8SEYr8oT_TDxKFX48'
